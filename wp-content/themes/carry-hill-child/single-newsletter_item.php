36759281b94466e69f76a9891c81d9011fd3fd82
Suzy Bates <suzy@fourkitchens.com>
2014-08-14 08:59:54 -0500

    New Newsletter structure updates

--- wp-content/themes/carry-hill-child/single-newsletter_item.php
+++ wp-content/themes/carry-hill-child/single-newsletter_item.php
@@ -8,9 +8,12 @@
 get_header();
 get_template_part('header-bottom');
 
-$pub_date  = do_shortcode('[pods]{@newsletter_date}[/pods]');
-$from_date = mysql2date("Y-m-d",do_shortcode('[pods]{@events_from_date}[/pods]'));
-$to_date   = mysql2date("Y-m-d",do_shortcode('[pods]{@events_to_date}[/pods]'));
+
+$newsletter_longer_description = do_shortcode('[pods]{@newsletter_longer_description}[/pods]');
+$show_on_home = do_shortcode('[pods]{@show_on_home_page}[/pods]');
+$home_page_short_description = do_shortcode('[pods]{@home_page_short_description}[/pods]');
+$from_date = mysql2date("Y-m-d",do_shortcode('[pods]{@start_post_date_on_home_page}[/pods]'));
+$to_date   = mysql2date("Y-m-d",do_shortcode('[pods]{@end_post_date_on_home_page}[/pods]'));
 
 
 ?>
@@ -21,6 +24,13 @@ $to_date   = mysql2date("Y-m-d",do_shortcode('[pods]{@events_to_date}[/pods]'));
             
             <?php /* The loop */ ?>
             <?php while (have_posts()) : the_post(); ?>
+            	<h2> Newsletter Content </h2>
+	            	<div><?php echo $newsletter_longer_description ?></div>
+            	<h2> Home Page Announcement Content </h2>
+	            	<div>Show on Homepage? <?php echo $show_on_home ?></div>	
+	            	<div><?php echo $home_page_short_description  ?></div>   
+	            	<div>From <?php echo $from_date  ?> to <?php echo $to_date ?></div>
+	            	           	
                 <?php get_template_part('content', get_post_format()); ?>
                 
                 <?php // twentythirteen_post_nav(); ?>
