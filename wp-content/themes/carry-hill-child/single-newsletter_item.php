4f327d5a9ba6edd3e7e3267df28f237c81c02591
Suzy Bates <suzy@fourkitchens.com>
2014-08-14 09:28:31 -0500

    Updates to the newsletter display

--- wp-content/themes/carry-hill-child/single-newsletter_item.php
+++ wp-content/themes/carry-hill-child/single-newsletter_item.php
@@ -14,7 +14,8 @@ $show_on_home = do_shortcode('[pods]{@show_on_home_page}[/pods]');
 $home_page_short_description = do_shortcode('[pods]{@home_page_short_description}[/pods]');
 $from_date = mysql2date("Y-m-d",do_shortcode('[pods]{@start_post_date_on_home_page}[/pods]'));
 $to_date   = mysql2date("Y-m-d",do_shortcode('[pods]{@end_post_date_on_home_page}[/pods]'));
-
+$newsletter_item_contact = do_shortcode('[pods]{@newsletter_item_contact}[/pods]');
+$contact_email = do_shortcode('[pods]{@contact_email}[/pods]');
 
 ?>
 <div class="cbp-row cbp_widget_row ch-content ch-content-top ch-content-bottom ch-content-top-rainbow">
@@ -24,21 +25,22 @@ $to_date   = mysql2date("Y-m-d",do_shortcode('[pods]{@end_post_date_on_home_page
             
             <?php /* The loop */ ?>
             <?php while (have_posts()) : the_post(); ?>
-            	<h2> Newsletter Content </h2>
+            	<h3> On the Newsletter: </h3>
+            	<h4><?php get_template_part('partials/page-title'); ?></h4>
 	            	<div><?php echo $newsletter_longer_description ?></div>
-            	<h2> Home Page Announcement Content </h2>
-	            	<div>Show on Homepage? <?php echo $show_on_home ?></div>	
-	            	<div><?php echo $home_page_short_description  ?></div>   
-	            	<div>From <?php echo $from_date  ?> to <?php echo $to_date ?></div>
-	            	           	
+	            <?php if ((int) $show_on_home): ?>
+            		<h3> On the Home Page</h3>
+	            		<div><?php echo $home_page_short_description  ?></div>   
+	            		<div>From <?php echo $from_date  ?> to <?php echo $to_date ?></div>
+	            <?php endif ?>	           	
                 <?php get_template_part('content', get_post_format()); ?>
                 
-                <?php // twentythirteen_post_nav(); ?>
-                <?php comments_template(); ?>
             <?php endwhile; ?>
         </div>
         <div class="one third double-padded">
-           <?php //get_sidebar(); ?>
+           <h4>Questions about this post? </h4>
+           <div>Contact: <?php echo $newsletter_item_contact ?></div>
+           <div>Email <?php echo $contact_email ?></div>
         </div>
     </div>
 </div>
