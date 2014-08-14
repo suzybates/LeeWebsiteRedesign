684695c8319b3931f2d098dcd70ab24136bfb2e0
Suzy Bates <suzy@fourkitchens.com>
2014-08-14 09:53:31 -0500

    More 3 newsletter updates

--- wp-content/themes/carry-hill-child/single-newsletter_item.php
+++ wp-content/themes/carry-hill-child/single-newsletter_item.php
@@ -12,8 +12,8 @@ get_template_part('header-bottom');
 $newsletter_longer_description = do_shortcode('[pods]{@newsletter_longer_description}[/pods]');
 $show_on_home = do_shortcode('[pods]{@show_on_home_page}[/pods]');
 $home_page_short_description = do_shortcode('[pods]{@home_page_short_description}[/pods]');
-$from_date = mysql2date("Y-m-d",do_shortcode('[pods]{@start_post_date_on_home_page}[/pods]'));
-$to_date   = mysql2date("Y-m-d",do_shortcode('[pods]{@end_post_date_on_home_page}[/pods]'));
+$from_date = do_shortcode('[pods]{@start_post_date_on_home_page}[/pods]');
+$to_date   = do_shortcode('[pods]{@end_post_date_on_home_page}[/pods]');
 $newsletter_item_contact = do_shortcode('[pods]{@newsletter_item_contact}[/pods]');
 $contact_email = do_shortcode('[pods]{@contact_email}[/pods]');
 
@@ -26,10 +26,9 @@ $contact_email = do_shortcode('[pods]{@contact_email}[/pods]');
             <?php /* The loop */ ?>
             <?php while (have_posts()) : the_post(); ?>
             	<h3> On the Newsletter: </h3>
-            	<h4><?php get_template_part('page-title'); ?></h4>
-	            	<div><?php echo $newsletter_longer_description ?></div>
+            	<div><?php echo $newsletter_longer_description ?></div>
 	            <?php if ($show_on_home): ?>
-            		<h3> On the Home Page</h3>
+            		<h3> On the Home Page:</h3>
 	            		<div><?php echo $home_page_short_description  ?></div>   
 	            		<div>From <?php echo $from_date  ?> to <?php echo $to_date ?></div>
 	            <?php endif ?>	           	
@@ -40,7 +39,7 @@ $contact_email = do_shortcode('[pods]{@contact_email}[/pods]');
         <div class="one third double-padded">
            <h4>Questions about this post? </h4>
            <div>Contact: <?php echo $newsletter_item_contact ?></div>
-           <div>Email <?php echo $contact_email ?></div>
+           <div>Email: <?php echo $contact_email ?></div>
         </div>
     </div>
 </div>
